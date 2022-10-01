62d6b8a9f197ff6d557f74133950df41941614f3
Christopher Walker <hello@chrisashwalker.com>
2022-10-01 17:27:30 +0100

    Use React-Bootstrap components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import CardLink from './components/CardLink/CardLink';
 
 function App() {
     useEffect(() => {
         const counter: number = Math.floor(Math.random() * 6) + 1;
-        const bgDiv: HTMLDivElement | null = document.querySelector<HTMLDivElement>("#bg");
+        const bgDiv: HTMLElement | null = document.querySelector<HTMLElement>(".cw-bg");
         if (bgDiv) {
             bgDiv.style.width = "100%";
             bgDiv.style.height = "100%";
@@ -17,27 +19,25 @@ function App() {
     });
 
     return (
-        <div id="bg">
-            <main className="container">
-                <section className="row row-cols-1 row-cols-md-1 row-cols-lg-3 text-center min-vh-100 align-items-center pb-4">
-                    <CardLink 
-                        img="instagramSnip.png" 
-                        url="https://instagram.com/chrisashwalker" 
-                        text="instagram.com/chrisashwalker" 
-                    />
-                    <CardLink 
-                        img="linkedinSnip.png" 
-                        url="https://linkedin.com/in/chrisashwalker" 
-                        text="linkedin.com/in/chrisashwalker" 
-                    />
-                    <CardLink 
-                        img="githubSnip.png" 
-                        url="https://github.com/chrisashwalker" 
-                        text="github.com/chrisashwalker" 
-                    />
-                </section>
-            </main>
-        </div>
+        <Container fluid className="cw-bg">
+            <Row xs={1} lg={3} className="min-vh-100 align-items-center pb-4">
+                <CardLink
+                    img="instagramSnip.png"
+                    url="https://instagram.com/chrisashwalker"
+                    text="instagram.com/chrisashwalker"
+                />
+                <CardLink
+                    img="linkedinSnip.png"
+                    url="https://linkedin.com/in/chrisashwalker"
+                    text="linkedin.com/in/chrisashwalker"
+                />
+                <CardLink
+                    img="githubSnip.png"
+                    url="https://github.com/chrisashwalker"
+                    text="github.com/chrisashwalker"
+                />
+            </Row>
+        </Container>
     )
 }
 
--- src/components/CardLink/CardLink.tsx
+++ src/components/CardLink/CardLink.tsx
@@ -1,20 +1,22 @@
 import "./CardLink.css";
 
+import { Card, Col } from "react-bootstrap";
+
 const CardLink = (props: {img: string, url: string, text: string}) => {
 
     return (
-        <div className="col mt-4">
-            <div className="card bg-secondary cw-hover">
-                <img className="card-img-top" src={props.img} />
-                <div className="card-body">
-                    <p className="card-text">
-                        <a className="stretched-link text-light" href={props.url}>
+        <Col className="mt-4">
+            <Card bg="dark" text="light" className="cw-card-link m-auto">
+                <Card.Img variant="top" src={props.img} alt={props.text} />
+                <Card.Body>
+                    <Card.Text className="text-center">
+                        <Card.Link className="stretched-link text-light" href={props.url}>
                             {props.text}
-                        </a>
-                    </p>
-                </div>
-            </div>
-        </div>
+                        </Card.Link>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
     )
 
 }
